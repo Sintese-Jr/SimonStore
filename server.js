a879bff275295111db8acc2b5c344a452c87dec0
LeoPocas <84936221+LeoPocas@users.noreply.github.com>
2022-08-23 14:02:45 -0300

    Ajuste da rota para teste no git pages
    Atualização ainda não testada na integração com o git pages

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 
-app.get('/', function(req, res){
+app.get('/SimonStore', function(req, res){
     res.sendFile(__dirname+"/index.html")
 })
 
